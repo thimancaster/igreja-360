3daa9e3d4bbadb26f9c078cb514b77fbdcafa78c
[dyad] <thimancaster@hotmail.com>
2025-11-06 11:21:10 -0300

    [dyad] Adicionando Error Boundary para tratamento de erros na UI - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ErrorBoundary from "./components/ErrorBoundary.tsx"; // Importar ErrorBoundary
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
